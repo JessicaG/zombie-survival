983ab400caa0297c0880580d5f75dae344beb792
jgoulding2@gmail.com <jgoulding2@gmail.com>
2012-05-31 23:59:48 -0600

    last

--- initialprompt.js
+++ initialprompt.js
@@ -1,13 +1,16 @@
 var timeofday = function(){
-	var timeofday = prompt ("What time is it?");
-	if (timeofday >=3pm && timeofday<6pm) {
+	var timeofday = prompt ("What time is it? Note: must be in miliatry style");
+	if (timeofday >=15 && timeofday<17) {
 	 result = "You need to wait until dusk to move to the nearest neighborhood";
 	}
-	if (timeofday >=5pm && timeofday<9pm) {
+	if (timeofday >=17 && timeofday<21) {
 		 result = "Time to make your move, go find the house with the white picket fence.";
 		}
-	if (timeof day <9pm>){
+	if (timeofday <21>){
 		result ="Eeeps, cutting it close there beauty queen! Tomorrow night, less time in the shower, more time thinking about flesh eating. Go on though, run your arse off to the closest house.";
 	}
+	if (timeofday.length ===0 || timeofday >100 ){
+			result = "Hold up, you gotta enter a number between 1 - 24."
+		}
 	alert (result);
 }
